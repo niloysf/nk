83448c88631a5196d0221cd9e014022cd5fe2867
niloysf <nilay15-12203@diu.edu.bd>
2022-01-26 21:29:19 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,5 @@
+var friendsAge = [11, 13, 17];
+var muriChanachur = [34, 55, 221, 3];
+var nayikas = ['mahi', 'opu', 'sabana'];
+var oddNumbers = [1, 3, 5, 7, 9];
+var vowles = ['a', 'e', 'i', 'o', 'u'];
---
+++ variable.js
@@ -0,0 +1,8 @@
+var watchPrice = 250;
+watchPrice = 150;
+console.log(watchPrice);
+// string
+var userName = "sogir Uddin";
+// Boolean
+var isExpensive = true;
+console.log(isExpensive);
